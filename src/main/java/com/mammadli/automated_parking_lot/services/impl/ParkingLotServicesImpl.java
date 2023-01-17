e3b200dfa42a1a3592e9a7ecec24b7d2f6cb05b2
mammadli <gulnar.mammadli@ut.ee>
2023-01-17 20:53:20 +0200

    create method updated

--- src/main/java/com/mammadli/automated_parking_lot/services/impl/ParkingLotServicesImpl.java
+++ src/main/java/com/mammadli/automated_parking_lot/services/impl/ParkingLotServicesImpl.java
@@ -15,21 +15,17 @@ import java.util.Optional;
 public class ParkingLotServicesImpl implements ParkingLotServices {
     public static int SUCCESS_CODE = 200;
     public static int NOT_FOUND_CODE = 404;
-    public static int ALREADY_EXIST_CODE = 409;
     public static String SUCCESS_MESSAGE = "SUCCESS";
     public static String NOT_FOUND_MESSAGE = "NOT_FOUND";
-    public static String ALREADY_EXIST_MESSAGE = "ALREADY EXIST";
 
     private final ParkingLotRepository parkingLotRepository;
     @Override
     public ResponseData<ParkingLot> create(ParkingLot parkingLot) {
 
         ParkingLot newParkingLot = parkingLotRepository.save(parkingLot);
-        if(newParkingLot != null) {
-            return GenerateResponseUtility.parkingLotFunc.generate(SUCCESS_CODE, SUCCESS_MESSAGE, newParkingLot);
-        }
-        return GenerateResponseUtility.parkingLotFunc.generate(ALREADY_EXIST_CODE,ALREADY_EXIST_MESSAGE,null);
+        return GenerateResponseUtility.parkingLotFunc.generate(SUCCESS_CODE, SUCCESS_MESSAGE, newParkingLot);
     }
+
     @Override
     public ResponseData<String> delete(String id) {
 
