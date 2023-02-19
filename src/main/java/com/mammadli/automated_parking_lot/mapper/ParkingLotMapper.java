f48b8f5ddf297386a4b882bfe1e6590d86c18b6b
mammadli <gulnar.mammadli@ut.ee>
2023-02-19 12:49:36 +0200

    Add PArkingLot Mapper

---
+++ src/main/java/com/mammadli/automated_parking_lot/mapper/ParkingLotMapper.java
@@ -0,0 +1,17 @@
+package com.mammadli.automated_parking_lot.mapper;
+
+import com.mammadli.automated_parking_lot.db.dto.ParkingLotDto;
+import com.mammadli.automated_parking_lot.db.entity.ParkingLot;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface ParkingLotMapper {
+
+    ParkingLotMapper INSTANCE = Mappers.getMapper(ParkingLotMapper.class);
+
+    ParkingLot toParkingLot(ParkingLotDto parkingLotDto);
+
+    ParkingLotDto toParkingLotDto(ParkingLot parkingLot);
+
+}
