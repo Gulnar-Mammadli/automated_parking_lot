018fc20e95b9defb258a64304a8f7d357c34a43b
mammadli <gulnar.mammadli@ut.ee>
2023-02-19 13:01:29 +0200

    Adding constructors

--- src/main/java/com/mammadli/automated_parking_lot/db/dto/FloorDto.java
+++ src/main/java/com/mammadli/automated_parking_lot/db/dto/FloorDto.java
@@ -1,11 +1,21 @@
 package com.mammadli.automated_parking_lot.db.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FloorDto {
+
     private int ceilingHeight;
+
     private int weightCapacity;
+
     private int maxCapacity;
+
     private String parkingLotId;
 }
