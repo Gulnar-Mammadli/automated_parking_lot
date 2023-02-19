35a4cfd7fefa848f33868709417ef99205c32f23
mammadli <gulnar.mammadli@ut.ee>
2023-02-19 13:14:12 +0200

    Update create and delete endpoints

--- src/main/java/com/mammadli/automated_parking_lot/services/impl/ParkingLotServicesImpl.java
+++ src/main/java/com/mammadli/automated_parking_lot/services/impl/ParkingLotServicesImpl.java
@@ -1,10 +1,10 @@
 package com.mammadli.automated_parking_lot.services.impl;
 
+import com.mammadli.automated_parking_lot.db.dto.ParkingLotDto;
 import com.mammadli.automated_parking_lot.db.entity.ParkingLot;
 import com.mammadli.automated_parking_lot.db.repository.ParkingLotRepository;
+import com.mammadli.automated_parking_lot.mapper.ParkingLotMapper;
 import com.mammadli.automated_parking_lot.services.ParkingLotServices;
-import com.mammadli.automated_parking_lot.util.GenerateResponseUtility;
-import com.mammadli.automated_parking_lot.util.ResponseData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,27 +13,22 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class ParkingLotServicesImpl implements ParkingLotServices {
-    public static int SUCCESS_CODE = 200;
-    public static int NOT_FOUND_CODE = 404;
-    public static String SUCCESS_MESSAGE = "SUCCESS";
-    public static String NOT_FOUND_MESSAGE = "NOT_FOUND";
 
     private final ParkingLotRepository parkingLotRepository;
-    @Override
-    public ResponseData<ParkingLot> create(ParkingLot parkingLot) {
 
-        ParkingLot newParkingLot = parkingLotRepository.save(parkingLot);
-        return GenerateResponseUtility.parkingLotFunc.generate(SUCCESS_CODE, SUCCESS_MESSAGE, newParkingLot);
+    @Override
+    public ParkingLot create(ParkingLotDto parkingLotDto) {
+
+        return parkingLotRepository.save(ParkingLotMapper.INSTANCE.toParkingLot(parkingLotDto));
     }
 
     @Override
-    public ResponseData<String> delete(String id) {
+    public Void delete(String id) {
 
         Optional<ParkingLot> result = parkingLotRepository.findById(id);
-        if(result.isPresent()){
-            parkingLotRepository.deleteById(result.get().getId());
-            return GenerateResponseUtility.func.generate(SUCCESS_CODE,SUCCESS_MESSAGE,null);
+        if (result.isPresent()) {
+            parkingLotRepository.deleteById(id);
         }
-        return GenerateResponseUtility.func.generate(NOT_FOUND_CODE,NOT_FOUND_MESSAGE,null);
+        return null;
     }
 }
